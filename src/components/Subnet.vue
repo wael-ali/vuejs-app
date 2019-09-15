f2660a6070002574f532bdc42e06f08439b90ff7
wael-ali <waelali595@gmail.com>
2019-09-15 04:54:41 +0200

    calculations for mask and hosts to ent work

--- src/components/Subnet.vue
+++ src/components/Subnet.vue
@@ -38,15 +38,35 @@
         <div type="submit" class="btn btn-primary mt-4" @click="claculateHosts">Calculate</div>
       </form>
 
-
-
       <div class="mt-3">
         <div class="alert alert-info">
-          <div>{{ binaryMask }}</div>
-          <div>{{ hosts }}</div>
-          <div> {{ netClass }}</div>
+          <div class="flex-info">
+            <div>Network Mask:</div> 
+            <div>{{ decimalMask }}</div>
+          </div>
+
+          <div class="flex-info">
+            <div>IP: </div>
+            <div>{{ maskOctet1 }}.{{ maskOctet2 }}.{{ maskOctet3 }}.{{ maskOctet4 }}</div>
+          </div>
+
+          <div class="flex-info">
+            <div>IP in binary: </div>
+            <div>{{ binaryMask }}</div>
+          </div>
+
+          <div class="flex-info">
+            <div>Number of Hosts: </div>
+            <div>{{ hosts }}</div>
+          </div>
+
+          <div class="flex-info">
+            <div> net Class</div>
+            <div>{{ netClass }}</div>
+          </div>
         </div>
       </div>
+
       <div class="mt-3">
         <div class="alert alert-warning">
           <div>{{ test }}</div>
@@ -63,113 +83,179 @@ export default {
   props: {
     msg: String
   },
-  data(){
-    return{
-        binary_ip: [],
-        network_number: '',
-        network_number_error: false,
-        network_number_error_msg: 'number between 0-30',
-        maskOctet1: '',
-        maskOctet1_error: false,
-        maskOctet2: '',
-        maskOctet2_error: false,
-        maskOctet3: '',
-        maskOctet3_error: false,
-        maskOctet4: '',
-        maskOctet4_error: false,
-        netClass: 'NetClass',
-        hosts: 'number of hosts',
-        binaryMask: 'binary mask',
-        test: 'Testing Area',
-        error_msg: 'number between 0-255',
+  data () {
+    return {
+      binary_ip: [],
+      network_number: '',
+      network_number_error: false,
+      network_number_error_msg: 'number between 0-30',
+      maskOctet1: '',
+      maskOctet1_error: false,
+      maskOctet2: '',
+      maskOctet2_error: false,
+      maskOctet3: '',
+      maskOctet3_error: false,
+      maskOctet4: '',
+      maskOctet4_error: false,
+      netClass: 'NetClass',
+      hosts: 'number of hosts',
+      binaryMask: 'binary mask',
+      decimalMask: 'decimal mask',
+      test: 'Testing Area',
+      error_msg: 'number between 0-255',
     }
   },
   methods: {
-      claculateHosts() {
-//            this.hosts = this.maskOctet1 +'.'+ this.maskOctet2 +'.'+ this.maskOctet3 +'.'+ this.maskOctet4
-//            this.hosts = this.maskOctet1.toString(2)
-          this.validateInput();
-          if(this.maskOctet1_error || this.maskOctet2_error || this.maskOctet3_error || this.maskOctet4_error || this.network_number_error){
-              ;
-          }
-          this.hosts = (this.maskOctet1 >>> 0).toString(2)
-              + '.' + (this.maskOctet2 >>> 0).toString(2)
-              + '.' + (this.maskOctet3 >>> 0).toString(2)
-              + '.' + (this.maskOctet4 >>> 0).toString(2)
+    claculateHosts () {
+      this.convertToBinaryWord(10);
+      // // this.validateInput();
+      // if (this.maskOctet1_error || this.maskOctet2_error || this.maskOctet3_error || this.maskOctet4_error || this.network_number_error){
+      //   this.resetValues();
+      //   return;
+      // }else{
 
-          this.binary_ip.push((this.maskOctet1 >>> 0).toString(2))
-          this.binary_ip.push((this.maskOctet2 >>> 0).toString(2))
-          this.binary_ip.push((this.maskOctet3 >>> 0).toString(2))
-          this.binary_ip.push((this.maskOctet4 >>> 0).toString(2))
+        this.binaryMask = this.convertToBinaryWord(this.maskOctet1)
+          + '.' + this.convertToBinaryWord(this.maskOctet2)
+          + '.' + this.convertToBinaryWord(this.maskOctet3)
+          + '.' + this.convertToBinaryWord(this.maskOctet4)
 
-          this.binaryMask = (this.maskOctet1 >>> 0).toString(2)
-              + '.' + (this.maskOctet2 >>> 0).toString(2)
-              + '.' + (this.maskOctet3 >>> 0).toString(2)
-              + '.' + (this.maskOctet4 >>> 0).toString(2)
+        // this.binaryMask =  this.convertToBinaryWord(this.maskOctet1)
+        // + this.convertToBinaryWord(this.maskOctet2)
+        // + this.convertToBinaryWord(this.maskOctet3)
+        // + this.convertToBinaryWord(this.maskOctet4)
 
-          this.netClass = (this.maskOctet1 >>> 0).toString(2).substring(0, 3)
+        this.getNetworkMask()
+        this.getHostsNumber()
 
-          this.test = (this.maskOctet1 >>> 0).toString(2)
-              + (this.maskOctet2 >>> 0).toString(2)
-              + (this.maskOctet3 >>> 0).toString(2)
-              + (this.maskOctet4 >>> 0).toString(2)
+      //   this.netClass = (this.maskOctet1 >>> 0).toString(2).substring(0, 3)
 
-//          this.test += ' length: '+this.test.length
-//          this.test += 'UUU: ' + (parseInt(this.network_number) - 1)
-          this.test = this.test.substring(parseInt(this.network_number)-1)
-      },
-      validateInput() {
-//          if (this.maskOctet1 === "") {
-//              this.maskOctet1_error = true;
-//              this.test = 'is empty: ' + this.maskOctet1;
-//          }
+      //   this.test = (this.maskOctet1 >>> 0).toString(2)
+      //     + (this.maskOctet2 >>> 0).toString(2)
+      //     + (this.maskOctet3 >>> 0).toString(2)
+      //     + (this.maskOctet4 >>> 0).toString(2)
+      //   this.test = this.test.substring(parseInt(this.network_number)-1)
+      // }
+    },
+    validateInput() {
 
-          if (isNaN(this.maskOctet1) || this.maskOctet1 < 0 || this.maskOctet1 > 255 || this.maskOctet1.length === 0) {
-              this.maskOctet1_error = true;
-          } else {
-              this.maskOctet1_error = false;
-          }
-          if (isNaN(this.maskOctet2) || this.maskOctet2 < 0 || this.maskOctet2 > 255 || this.maskOctet2.length === 0) {
-              this.maskOctet2_error = true;
-          } else {
-              this.maskOctet2_error = false;
-          }
-          if (isNaN(this.maskOctet3) || this.maskOctet3 < 0 || this.maskOctet3 > 255 || this.maskOctet3.length === 0) {
-              this.maskOctet3_error = true;
-          } else {
-              this.maskOctet3_error = false;
-          }
-          if (isNaN(this.maskOctet4) || this.maskOctet4 < 0 || this.maskOctet4 > 255 || this.maskOctet4.length === 0) {
-              this.maskOctet4_error = true;
-          } else {
-              this.maskOctet4_error = false;
-          }
-          if (isNaN(this.network_number) || this.network_number < 0 || this.network_number > 255 || this.network_number.length === 0) {
-              this.network_number_error = true;
-          } else {
-              this.network_number_error = false;
-          }
-      },
-
-      resetValues(){
-
+      if (isNaN(this.maskOctet1) || this.maskOctet1 < 0 || this.maskOctet1 > 255 || this.maskOctet1.length === 0) {
+        this.maskOctet1_error = true
+      } else {
+        this.maskOctet1_error = false
       }
+      if (isNaN(this.maskOctet2) || this.maskOctet2 < 0 || this.maskOctet2 > 255 || this.maskOctet2.length === 0) {
+        this.maskOctet2_error = true
+      } else {
+        this.maskOctet2_error = false
+      }
+      if (isNaN(this.maskOctet3) || this.maskOctet3 < 0 || this.maskOctet3 > 255 || this.maskOctet3.length === 0) {
+        this.maskOctet3_error = true
+      } else {
+        this.maskOctet3_error = false
+      }
+      if (isNaN(this.maskOctet4) || this.maskOctet4 < 0 || this.maskOctet4 > 255 || this.maskOctet4.length === 0) {
+        this.maskOctet4_error = true
+      } else {
+        this.maskOctet4_error = false
+      }
+      if (isNaN(this.network_number) || this.network_number < 0 || this.network_number > 255 || this.network_number.length === 0) {
+        this.network_number_error = true
+      } else {
+        this.network_number_error = false
+      }
+    },
+
+    resetValues(){
+      this.binary_ip = []
+      this.network_number = ''
+      // this.network_number_error = false
+      this.network_number_error_msg = 'number between 0-30'
+      this.maskOctet1 = ''
+      // this.maskOctet1_error = false
+      this.maskOctet2 = ''
+      // this.maskOctet2_error = false
+      this.maskOctet3 = ''
+      // this.maskOctet3_error = false
+      this.maskOctet4 = ''
+      // this.maskOctet4_error = false
+      this.netClass = 'NetClass'
+      this.hosts = 'number of hosts'
+      this.binaryMask = 'binary mask'
+      this.test = 'Testing Area'
+      this.error_msg = 'number between 0-255'
+    },
+
+    convertToBinaryWord(value){
+      let binary = (value >>> 0).toString(2);
+      const length = binary.length
+      if (length < 8){
+        let zeros = '';
+        for (let index = 0; index < 8 - length; index++) {
+          zeros += '0';
+        }
+        zeros = zeros.concat(binary)
+        return zeros;
+      }
+      return binary
+    },
+
+    getNetworkMask(){
+      let bits_32 = this.convertToBinaryWord(this.maskOctet1)
+        + this.convertToBinaryWord(this.maskOctet2)
+        + this.convertToBinaryWord(this.maskOctet3)
+        + this.convertToBinaryWord(this.maskOctet4)
+      const host_bits = bits_32.substr(this.network_number)
+      this.test = host_bits
+      let mask = '';
+      let deci_mask = '';
+      let tep_word = '';
+      for (let i = 1; i <= (bits_32.length); i++) {
+        if (bits_32.length - host_bits.length < i) {
+          mask += '0';
+          tep_word += '0'
+          
+          if (i % 8 === 0) {
+            deci_mask += parseInt(tep_word, 2) + '.'
+            tep_word = ''
+          }
+        }else{
+          mask += '1';
+          tep_word += '1'
+          if (i % 8 === 0) {
+            deci_mask += parseInt(tep_word, 2) + '.'
+            tep_word = ''
+          }
+        }
+      }
+      console.log('parseint 255: ',mask, deci_mask)
+      this.decimalMask = deci_mask.substring(0, deci_mask.length - 1)
+    },
+
+    getHostsNumber(){
+      let bits_32 = this.convertToBinaryWord(this.maskOctet1)
+        + this.convertToBinaryWord(this.maskOctet2)
+        + this.convertToBinaryWord(this.maskOctet3)
+        + this.convertToBinaryWord(this.maskOctet4)
+      const host_bits = bits_32.substr(this.network_number)
+      this.test = host_bits
+      this.hosts = Math.pow(2, host_bits.length) - 2
+    },
   },
 
   computed: {
 
-      show_octetErrormsg(){
-          if (this.maskOctet1_error || this.maskOctet2_error || this.maskOctet3_error || this.maskOctet4_error ){
-              return true;
-          }
-          return false;
-      },
-      show_networkNumberErrormsg(){
-          if (this.network_number_error){
-              return true;
-          }
-          return false;
+    show_octetErrormsg(){
+      if (this.maskOctet1_error || this.maskOctet2_error || this.maskOctet3_error || this.maskOctet4_error ){
+        return true
       }
+      return false
+    },
+    show_networkNumberErrormsg(){
+      if (this.network_number_error){
+        return true
+      }
+      return false
+    }
   },
 }
 </script>
@@ -211,14 +297,20 @@ export default {
     width: 20%;
   }
 
-.error{
-  padding: 5px;
-  color: crimson;
-  border: 1px solid crimson;
-  border-radius: 4px;
-  text-align: center;
+  .error{
+    padding: 5px;
+    color: crimson;
+    border: 1px solid crimson;
+    border-radius: 4px;
+    text-align: center;
 
-}
+  }
 
+  .flex-info {
+    display: flex
+  }
+  .flex-info > div{
+    flex: 2;
+  }
 
 </style>
